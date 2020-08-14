7d67cb2f93235e0bfd589db6250899883cfd574d
Andrey Verbitskiy <verb2310@gmail.com>
2020-08-14 22:41:21 +0300

    Remove base

--- build-run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-clear && go build -o app . && ./app
--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,3 @@
 #!/usr/bin/env sh
 
-set -e
-
-ENV=${ENV:-local}
-echo "ENV: ${ENV}"
-
-#docker network create public || true
-cd docker
-
-if [ "$BUILD" = 1 ]
-then
-    docker-compose kill
-    docker-compose rm -f
-    docker-compose pull
-    docker-compose up --build -d
-else
-    docker-compose kill
-    docker-compose rm -f
-    docker-compose pull
-    docker-compose up -d
-fi
+clear && go build -o app . && ./app
